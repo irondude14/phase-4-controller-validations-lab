56399e7b74383e03dd48116ae0f16220ef9aa6c1
Vladyslav <vkadyrov@protonmail.com>
2023-04-15 15:38:25 -0400

    commit tree 43398b1aa8ef1203bed4c55e3575ee682d2c5d56

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,16 +1,15 @@
 class PostsController < ApplicationController
-
+  rescue_from ActiveRecord::RecordInvalid,
+              with: :render_unprocessable_entity_response
   def show
     post = Post.find(params[:id])
-    
+
     render json: post
   end
 
   def update
     post = Post.find(params[:id])
-
-    post.update(post_params)
-
+    post.update!(post_params)
     render json: post
   end
 
@@ -20,4 +19,12 @@ class PostsController < ApplicationController
     params.permit(:category, :content, :title)
   end
 
+  private
+
+  def render_unprocessable_entity_response(invalid)
+    render json: {
+             errors: invalid.record.errors,
+           },
+           status: :unprocessable_entity
+  end
 end
